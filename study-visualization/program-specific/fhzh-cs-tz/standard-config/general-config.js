ef6bbd760baf37852cec8a1220ab263ef69955c0
Kristian Lubina <kristian.lubina@outlook.com>
2026-03-14 14:08:52 +0100

    Update general-config.js

--- study-visualization/program-specific/fhzh-cs-tz/standard-config/general-config.js
+++ study-visualization/program-specific/fhzh-cs-tz/standard-config/general-config.js
@@ -10,5 +10,13 @@ window.StudiengangGeneralConfig = {
         <br>
       <a href="https://www.zhaw.ch/storage/engineering/studium/bachelorstudium/studiengaenge/informatik/modultafeln/Modultafel_IT_TZ_DE.pdf" target="_blank" rel="noopener">Modultafel (PDF)</a>
       <br>
-      <a href="../../Modulgruppen-Informatik-ZHAW.pdf" target="_blank" rel="noopener">Modulgruppen (PDF)</a>`,
+    <a href="../../Modulgruppen-Informatik-ZHAW.pdf" target="_blank" rel="noopener">Modulgruppen (PDF)</a>
+    <br>
+    <a href="https://stundenplan.zhaw.ch/Default.aspx" target="_blank" rel="noopener">Stundenplan Standard</a>
+    <br>
+    <a href="https://stundenplan.zhaw.ch/Default.aspx?SelCls=IT25ta_WIN-1" target="_blank" rel="noopener">1. Semester</a>
+    <br>
+    <a href="https://stundenplan.zhaw.ch/Default.aspx?SelCls=IT25ta_WIN-1#search" target="_blank" rel="noopener">2. Semester</a>
+    <br>
+    <a href="https://eventoweb.zhaw.ch/Evt_Pages/SuchResultat.aspx?node=c594e3e5-cd9a-4204-9a61-de1e43ccb7b0&Tabkey=WebTab_ModuleSuchenZHAW" target="_blank" rel="noopener">Module Suchen</a>`,
 };
